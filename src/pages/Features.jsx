7b7fc628396960f1e5b3c19fe6040f9ac831965a
frontendmoe <frontendmoe@gmail.com>
2023-08-10 23:46:12 +0100

    Fixes

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -4,7 +4,7 @@ import Join from '../components/Join'
 function Features() {
   return (
     <div>
-      <section className="lg:px-[180px]  topSection bg-[#EBE1C7] space-y-[30px] flex flex-col justify-center">
+      <section className="sectionPadding  topSection bg-[#EBE1C7] space-y-[30px] flex flex-col justify-center">
         <p className="text-[50.99px] font-[900] lg:w-[700.984px]">
           Empowering & Connecting All Members of the Cheer Community.
         </p>
@@ -15,14 +15,31 @@ function Features() {
       </section>
       <section className="bg-[#E2D9C0] min-h-screen"></section>
       {/* Cheer Program */}
-      <section className="bg-[#F1DFB5] min-h-screen relative">
+      <section className="bg-[#F1DFB5] flex flex-col justify-center sectionPadding min-h-screen relative">
+        <p className="sectionTitle">
+          Cheer <br /> Program <br /> Staff
+        </p>
+        <p className="smText lg:w-[268px]">
+          Create a profile in OSIO to engage the cheer community, offer coaching
+          services to our network of{' '}
+        </p>
         <div className="absolute right-0 bottom-[10%]">
           {cheerProgramTriang}
         </div>
       </section>
 
       {/* Facility Manager */}
-      <section className="bg-[#FFF3D8] min-h-screen"></section>
+      <section className="bg-[#FFF3D8] sectionPadding min-h-screen">
+        <div className="min-h-[70vh] border-b-[3px] border-[#DBD3BA] flex flex-col justify-center">
+          <p className="sectionTitle ">
+            Facility <br /> Managers
+          </p>
+          <p className="smText md:w-[268px]">
+            Create a profile in OSIO to engage the cheer community, offer
+            coaching services to our network of{' '}
+          </p>
+        </div>
+      </section>
       <Join></Join>
     </div>
   )
