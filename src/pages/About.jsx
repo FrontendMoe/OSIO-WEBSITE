a3afad20088cb54361226d4af28cfe75f8e8acb9
frontendmoe <frontendmoe@gmail.com>
2023-08-11 00:19:15 +0100

    AboutSec

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,20 @@ import Join from '../components/Join'
 function About() {
   return (
     <div>
-      <section className="padding topSection"> About</section>
+      <section className="sectionPadding  topSection bg-[#EBE1C7] space-y-[30px] flex flex-col justify-center">
+        <p className="text-[50.99px] font-[900] lg:w-[700.984px]">
+          OUR SPORT <br /> IS OURS.
+        </p>
+        <p className="text-[22.231px] font-[400] lg:w-[750px]">
+          Create a profile in OSIO to engage the cheer community, offer coaching
+          services to our network of athletes and create a profile{' '}
+        </p>
+      </section>
+      <section className="sectionPadding  topSection bg-[#E2D9C0] space-y-[30px] flex flex-col justify-center">
+        <p className="text-[50.99px] font-[900] lg:w-[700.984px]">
+          PLACEHOLDERAWAITING COPY/CONTENT FEEDBACK FROM OSIO TEAM
+        </p>
+      </section>
       <Join></Join>
     </div>
   )
