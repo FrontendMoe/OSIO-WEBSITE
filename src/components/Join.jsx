4b488b803061b9c303cfcb685322329fb73c28c4
frontendmoe <frontendmoe@gmail.com>
2023-08-16 16:37:10 +0100

    Fixes

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -2,12 +2,12 @@ function Join() {
   return (
     <section className="bg-[#EBE1C7] py-[80px] md:py-[150px]">
       <div className="text-center mx-auto space-y-[50px] w-[90%] md:w-1/2">
-        <p className="text-[60px] font-[900]">Join the OSIO Community Today</p>
-        <p className="text-[23.268px] font-[400]">
+        <p className="sectionTitle">Join the OSIO Community Today</p>
+        <p className="mdText font-[400]">
           Create a profile in OSIO to engage the cheer community, offer coaching
           services to our network of{' '}
         </p>
-        <button className="py-[30px] text-[#FFF3D8] text-[25px] font-[700] px-[70px] bg-[#1D1D1D] rounded-[3px]">
+        <button className="md:py-[30px] text-[#FFF3D8] text-[20px] md:text-[25px] font-[700] py-[20px] px-[30px] md:px-[70px] bg-[#1D1D1D] rounded-[3px]">
           Download Today
         </button>
       </div>
